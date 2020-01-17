d0a6500380c888f4b6db6925a2d905a626693117
TTTTk84 <bjjt1290058@gn.iwasaki.ac.jp>
2020-01-18 04:57:08 +0900

    王将が回転する処理を追加

--- www/js/king.js
+++ www/js/king.js
@@ -1,1 +1,50 @@
 //王将の処理
+
+window.onload = () => {
+  main();
+};
+
+//main処理
+const main = () => {
+  const waitTime = 10000;
+  console.log(`${waitTime}ミリ秒カウントダウン`);
+  setTimeout(() => kingControl(), waitTime);
+};
+
+//加速度センサーを感知するまで、王将を回す。
+const kingControl = () => {
+  console.log("kingControl開始");
+  const obniz = new Obniz("7904-8026");
+  obniz.onconnect = async function() {
+    const motor = obniz.wired("DCMotor", { forward: 0, back: 1 });
+
+    motor.forward();
+
+    //10秒間ループする
+    loop(motor);
+  };
+};
+
+//10秒たつか、sensor反応したらぬける。
+const loop = motor => {
+  let second = 0;
+
+  const count = () => {
+    console.log(`${second}ループ`);
+    second++;
+
+    /*
+      10秒以内にゴミ箱に捨てた場合の処理。
+    */
+
+    if (second > 10) {
+      motor.stop();
+      return;
+    }
+
+    clearTimeout(id);
+    id = setTimeout(count, 1000);
+  };
+
+  let id = setTimeout(count, 1000);
+};
