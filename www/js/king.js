33b0db43282a7f5ed4496e63f78706341a480607
TTTTk84 <bjjt1290058@gn.iwasaki.ac.jp>
2020-01-29 10:11:25 +0900

    微修正

--- www/js/king.js
+++ www/js/king.js
@@ -8,7 +8,7 @@ const kingmain = () => {
 //加速度センサーを感知するまで、王将を回す。
 const kingControl = () => {
   console.log("kingControl開始");
-  const obnizA = new Obniz("7904-8026");
+  const obnizA = new Obniz("1690-3518");
   obnizA.onconnect = async function() {
     const motor = obnizA.wired("DCMotor", { forward: 0, back: 1 });
 
@@ -28,7 +28,7 @@ const loop = motor => {
     console.log(`${second}ループ`);
     second++;
 
-    var obnizB = new Obniz("1690-3518");
+    var obnizB = new Obniz("7904-8026");
     obnizB.onconnect = async function() {
       console.log("garbagebox接続");
       const sensor = obnizB.wired("GP2Y0A21YK0F", {
@@ -38,13 +38,13 @@ const loop = motor => {
       });
       sensor.start(function(distance) {
         console.log(distance);
-        if (distance < 250) {
+        if (distance < 200) {
           flg = 1;
         }
       });
     };
 
-    if (flg) {
+    if (second > 1 && flg == 1) {
       motor.stop();
       console.log("止まった");
       return;
