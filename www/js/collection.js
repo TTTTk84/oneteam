4a51692b3ec548a2246556c6bf856f6666431b21
TTTTk84 <bjjt1290058@gn.iwasaki.ac.jp>
2020-02-22 05:21:16 +0900

    apiのwood_nowによって、表示する画像を変える処理を追加

--- www/js/collection.js
+++ www/js/collection.js
@@ -3,34 +3,76 @@ const img = [
   "image/wood_test2.png",
   "image/wood_test3.png",
   "image/wood_test4.png",
-  "image/wood_test5.png"
+  "image/wood_test5.png",
+  "image/wood_test6.png"
 ];
-const count_max = img.length;
+const question_img = "image/question.jpg";
+let wood_text = [[]];
+
+const count_max = 6;
 let wood_select = "";
 let wood_box = document.getElementById("wood_box");
+let wood_release = 0;
 
-const make_wood = (img, count) => {
-  const wood = document.createElement("div");
-  wood.href = "";
-  wood.id = `wood${count}`;
-  wood.className = `wood `;
-  wood.style = `background-image: url(${img})`;
-  return wood;
+const request = new XMLHttpRequest();
+
+request.open("GET", "https://oneteamprojectapi.herokuapp.com/users/1", true);
+request.responseType = "json";
+request.send();
+
+request.onload = function() {
+  let wood_now = JSON.stringify(this.response);
+  wood_now = JSON.parse(wood_now).wood_now;
+  wood_release = wood_now - 1;
+  console.log(wood_release);
+  wood_text_init();
+  wood_action();
 };
 
-for (let i = 0; i < count_max; i++) {
-  const wood = make_wood(img[i], i + 1);
-  wood_box.appendChild(wood);
-}
+const wood_action = function() {
+  const make_wood = (img, count) => {
+    const wood = document.createElement("div");
+    wood.id = count;
+    wood.className = `wood`;
+    wood.style = `background-image: url(${img})`;
+    return wood;
+  };
 
-$(function() {
-  $(".wood").on("click", function() {
-    wood_select = $(this).attr("id");
-    $(".wrap").css("opacity", "0.1");
-    $(".wood_info").fadeIn("slow");
+  for (let i = 0; i < count_max; i++) {
+    const wood = make_wood(img[i], i);
+    wood_box.appendChild(wood);
+  }
+
+  for (let i = wood_release; i < count_max; i++) {
+    const wood = document.getElementById(`${i}`);
+    wood.className = "question";
+    wood.style = `background-image: url(${question_img})`;
+  }
+
+  $(function() {
+    $(".wood").on("click", function() {
+      wood_select = $(this).attr("id");
+      document.getElementById("wood_title").textContent =
+        wood_text[wood_select][0];
+      document.getElementById("wood_content").textContent =
+        wood_text[wood_select][1];
+      $(".wrap").css("opacity", "0.1");
+      $(".wood_info").fadeIn("slow");
+    });
+    $(".wood_info").on("click", function() {
+      $(".wrap").css("opacity", "1.0");
+      $(".wood_info").fadeOut("slow");
+    });
   });
-  $(".wood_info").on("click", function() {
-    $(".wrap").css("opacity", "1.0");
-    $(".wood_info").fadeOut("slow");
-  });
-});
+};
+
+const wood_text_init = () => {
+  wood_text = [
+    ["wood1", "wood1のテキスト"],
+    ["wood2", "wood2のテキスト"],
+    ["wood3", "wood3のテキスト"],
+    ["wood4", "wood4のテキスト"],
+    ["wood5", "wood5のテキスト"],
+    ["wood6", "wood6のテキスト"]
+  ];
+};
