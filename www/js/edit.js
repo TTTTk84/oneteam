3d0da822ed3bedfe96cce2641a69fd025b781044
TTTTk84 <bjjt1290058@gn.iwasaki.ac.jp>
2020-02-28 01:15:08 +0900

    urlの修正

--- www/js/edit.js
+++ www/js/edit.js
@@ -2,7 +2,7 @@ $(function() {
   $("#submit").click(function() {
     $.ajax({
       type: "PUT",
-      url: "https://oneteamprojectapi.herokuapp.com/user_configs",
+      url: "https://oneteamprojectapi.herokuapp.com/users/1/user_configs",
       data: { user_id: "1", mail: $('input[name="mail"]').val() }
     }).done(function(msg) {
       alert("保存されました。");
